8383f481052ca3c683f60735f4827478479bec75
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-08-04 12:49:41 +0100

    Upload original project from 2018

---
+++ HitTheMole.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Hit_the_Mole
+{
+	partial class HitTheMole
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HitTheMole));
+			this.scoreLabel = new System.Windows.Forms.Label();
+			this.startLabel = new System.Windows.Forms.Label();
+			this.timeLabel = new System.Windows.Forms.Label();
+			this.roundLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// scoreLabel
+			// 
+			this.scoreLabel.AutoSize = true;
+			this.scoreLabel.Enabled = false;
+			this.scoreLabel.Location = new System.Drawing.Point(5, 5);
+			this.scoreLabel.Name = "scoreLabel";
+			this.scoreLabel.Size = new System.Drawing.Size(47, 13);
+			this.scoreLabel.TabIndex = 1;
+			this.scoreLabel.Text = "Score: 0";
+			this.scoreLabel.Visible = false;
+			// 
+			// startLabel
+			// 
+			this.startLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.startLabel.Location = new System.Drawing.Point(12, 9);
+			this.startLabel.Name = "startLabel";
+			this.startLabel.Size = new System.Drawing.Size(776, 531);
+			this.startLabel.TabIndex = 2;
+			this.startLabel.Text = "CLICK ANYWHERE TO START";
+			this.startLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.startLabel.Click += new System.EventHandler(this.startLabel_Click);
+			// 
+			// timeLabel
+			// 
+			this.timeLabel.AutoSize = true;
+			this.timeLabel.Enabled = false;
+			this.timeLabel.Location = new System.Drawing.Point(5, 23);
+			this.timeLabel.Name = "timeLabel";
+			this.timeLabel.Size = new System.Drawing.Size(0, 13);
+			this.timeLabel.TabIndex = 3;
+			this.timeLabel.Visible = false;
+			// 
+			// roundLabel
+			// 
+			this.roundLabel.AutoSize = true;
+			this.roundLabel.Enabled = false;
+			this.roundLabel.Location = new System.Drawing.Point(746, 5);
+			this.roundLabel.Name = "roundLabel";
+			this.roundLabel.Size = new System.Drawing.Size(48, 13);
+			this.roundLabel.TabIndex = 4;
+			this.roundLabel.Text = "Round 1";
+			this.roundLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.roundLabel.Visible = false;
+			// 
+			// HitTheMole
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(800, 549);
+			this.Controls.Add(this.roundLabel);
+			this.Controls.Add(this.timeLabel);
+			this.Controls.Add(this.scoreLabel);
+			this.Controls.Add(this.startLabel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "HitTheMole";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Hit the Mole!";
+			this.Load += new System.EventHandler(this.HitTheMole_Load);
+			this.Click += new System.EventHandler(this.HitTheMole_Click);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.HitTheMole_MouseMove);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label scoreLabel;
+		private System.Windows.Forms.Label startLabel;
+		private System.Windows.Forms.Label timeLabel;
+		private System.Windows.Forms.Label roundLabel;
+	}
+}
+
---
+++ HitTheMole.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Hit_the_Mole
+{
+	public partial class HitTheMole : Form
+	{
+		private bool started = false;
+
+		private System.Windows.Forms.Timer roundTimer = new System.Windows.Forms.Timer();
+		private Stopwatch swRoundTimer = new Stopwatch();
+		private int roundTime = 5;
+
+		private System.Windows.Forms.Timer moleTimer = new System.Windows.Forms.Timer();
+
+		private Random rand = new Random();
+
+		private List<PictureBox> moles = new List<PictureBox>();
+		private List<Point> locations = new List<Point>();
+
+		private int score = 0;
+		PictureBox cursor = new PictureBox();
+
+		private int round = 1;
+
+		public HitTheMole()
+		{
+			InitializeComponent();
+
+			roundTimer.Interval = 1000;
+			roundTimer.Tick += new EventHandler(roundTimerTick);
+
+			moleTimer.Interval = 700;
+			moleTimer.Tick += new EventHandler(moleTimerTick);
+
+			cursor.Image = Properties.Resources.mousePress;
+			cursor.Parent = this;
+			cursor.Location = PointToClient(Cursor.Position);
+			cursor.SizeMode = PictureBoxSizeMode.StretchImage;
+			cursor.BackColor = Color.Transparent;
+			cursor.Visible = false;
+		}
+
+		private void HitTheMole_Load(object sender, EventArgs e)
+		{
+			//totalMoles = (roundTime * 1000)/moleTimer.Interval;
+			//scoreLabel.Text = "Score: 0/" + totalMoles;
+		}
+
+		private void reset()
+		{
+			started = false;
+
+			cursor.Visible = false;
+			
+			startLabel.Visible = true;
+			startLabel.Enabled = true;
+
+			scoreLabel.Enabled = false;
+			scoreLabel.Visible = false;
+
+			timeLabel.Enabled = false;
+			timeLabel.Visible = false;
+
+			roundLabel.Enabled = false;
+			roundLabel.Visible = false;
+
+			Cursor = Cursors.Default;
+
+			roundTimer.Stop();
+			swRoundTimer.Stop();
+			swRoundTimer.Reset();
+			moleTimer.Stop();
+
+			foreach (PictureBox mole in moles)
+			{
+				mole.Visible = false;
+				mole.Enabled = false;
+				mole.Dispose();
+			}
+		}
+
+		private void start()
+		{
+			started = true;
+			
+			startLabel.Visible = false;
+			startLabel.Enabled = false;
+
+			scoreLabel.Enabled = true;
+			scoreLabel.Visible = true;
+
+			timeLabel.Enabled = true;
+			timeLabel.Visible = true;
+
+			roundLabel.Enabled = true;
+			roundLabel.Visible = true;
+
+			MemoryStream stream = new MemoryStream(Properties.Resources.nothing);
+			//Cursor = new Cursor(stream);
+			//cursor.Visible = true;
+
+			roundTimer.Start();
+			swRoundTimer.Start();
+
+			moleTimer.Start();
+		}
+
+		private void moleTimerTick(object sender, EventArgs e)
+		{
+			int width = Properties.Resources.mole.Size.Width / 5;
+			int height = Properties.Resources.mole.Size.Height / 5;
+			Size size = new Size(width, height);
+
+			int xPos = rand.Next(80, Width-80);
+			int yPos = rand.Next(80, Height-80);
+			Point point = new Point(xPos, yPos);
+
+			/*foreach (Point loc in locations)
+			{
+				Console.WriteLine("X: " + (loc.X) + "[" + (loc.X+Width) + "], Y: " + (loc.Y) + " [" + (loc.Y+height) + "]");
+			}*/
+
+			PictureBox mole = new PictureBox();
+			mole.Click += new EventHandler(anyMoleClicked);
+			mole.Parent = this;
+			mole.Location = point;
+			mole.Size = size;
+			mole.SizeMode = PictureBoxSizeMode.StretchImage;
+			mole.Image = Properties.Resources.mole;
+			mole.BackColor = Color.Transparent;
+
+			moles.Add(mole);
+			//locations.Add(point);
+		}
+
+		private void anyMoleClicked(object mole, EventArgs e)
+		{
+			score += 1;
+			scoreLabel.Text = "Score: " + score; // + "/" + totalMoles;
+
+			PictureBox box = (PictureBox)mole;
+			box.Visible = false;
+			box.Enabled = false;
+			box.Dispose();
+		}
+
+		private void roundTimerTick(object sender, EventArgs e)
+		{
+			var seconds = swRoundTimer.ElapsedMilliseconds / 1000;
+
+			if (seconds >= roundTime+1)
+			{
+				reset();
+				MessageBox.Show("Round 1 has finished.\n\nScore: " + score + "\nTime: " + roundTime + "s.\nMoles spawned: " + moles.Count + ".\nSpawn interval: " + moleTimer.Interval + "ms.", "Times up!", MessageBoxButtons.OK);
+				score = 0;
+				scoreLabel.Text = "Score: 0";
+				return;
+			}
+			else
+			{
+				timeLabel.Text = "Time: " + seconds + "s/" + roundTime + "s";
+			}
+		}
+
+		private void HitTheMole_Click(object sender, EventArgs e)
+		{
+			if (!started) {
+				start();
+			}
+		}
+
+		private void startLabel_Click(object sender, EventArgs e)
+		{
+			if (!started)
+			{
+				start();
+			}
+		}
+
+		private void HitTheMole_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (started)
+			{
+				cursor.Location = PointToClient(Cursor.Position);
+			}
+		}
+	}
+}
